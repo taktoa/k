659837280d5cce9e0beec418f91592f281f08d1a
Dwight Guth <dwightguth@runtimeverification.com>
2015-05-19 14:35:01 -0500

    add rewriter interface documentation

--- kore/src/main/scala/org/kframework/Rewriter.scala
+++ kore/src/main/scala/org/kframework/Rewriter.scala
@@ -19,13 +19,36 @@ trait Rewriter {
    * - for search, it returns an Or with multiple ground terms as children
    */
 
+  /**
+   * Converts a term into a K which has been optimized for execution in the backend.
+   * @param k A K term to convert
+   * @return A K term which, when passed to other methods in this interface, does not require further conversion.
+   */
   def convert(k: K): K = k
 
+  /**
+   * Matches a particular subject against a pattern
+   * @param k The subject to match against.
+   * @param trace true if trace information about the matching should be printed to standard output.
+   * @param rule The rule to match and check the side condition of.
+   * @throws UnsupportedOperationException if the class does not support rule traces, and trace is true.
+   * @return A list of substitutions satisfying the rule.
+   */
   def `match`(k: K, trace: Boolean, rule: Rule): java.util.List[_ <: java.util.Map[_ <: KVariable, _ <: K]]
 
+  /**
+   * Substitutes a particular term using the right hand side of a rule.
+   * @param substitution A mapping of substitutions for variables in the rule.
+   * @param rule The rule whose right hand side should be used for the substitution
+   * @return the right hand side of rule substituted using the substitution.
+   */
   def substitute(substitution: java.util.Map[_ <: KVariable, _ <: K], rule: Rule): K
 
   def execute(k: K): K
 
+  /**
+   * Returns the list of rules in the definition being rewritten over, in the order they should be tried.
+   * These rules can be passed to other methods in this interface without further conversion.
+   */
   def rules : java.util.List[_ <: Rule]
 }
