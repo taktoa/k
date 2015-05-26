bc3bb7d8f6d9275edec6e06d5e73a3adf1696fac
Remy Goldschmidt <taktoa@gmail.com>
2015-05-26 13:34:05 -0500

    Refactor ConfigurationCleaner

--- kernel/src/main/java/org/kframework/compile/ConfigurationCleaner.java
+++ kernel/src/main/java/org/kframework/compile/ConfigurationCleaner.java
@@ -6,34 +6,42 @@ import org.kframework.kil.visitors.CopyOnWriteTransformer;
 import org.kframework.kil.*;
 import org.kframework.kil.Cell.Ellipses;
 import org.kframework.kil.Cell.Multiplicity;
+import org.kframework.utils.errorsystem.KEMException;
 import org.kframework.utils.errorsystem.KExceptionManager;
 
 public class ConfigurationCleaner extends CopyOnWriteTransformer {
 
     public ConfigurationCleaner(Context context) {
         super("Configuration Cleaner", context);
-        // TODO Auto-generated constructor stub
     }
 
+    private KEMException expectedCellError(Cell node) {
+        String errorStr = "Expecting Cell, but got "
+                        + node.getClass()
+                        + " in Configuration Cleaner.";
+        return KExceptionManager.internalError(errorStr, this, node);
+    }
+    
     @Override
     public ASTNode visit(Cell node, Void _void)  {
-        if (node.getMultiplicity() == Multiplicity.ANY || node.getMultiplicity() == Multiplicity.MAYBE) {
+        if (   node.getMultiplicity() == Multiplicity.ANY
+            || node.getMultiplicity() == Multiplicity.MAYBE) {
             if (node.variables().isEmpty()) {
                 return new Bag();
             }
         }
 
         ASTNode result = super.visit(node, _void);
-        if (result == null) return null;
+        if (result == null) { return null; }
 
         if (result == node) {
             node = node.shallowCopy();
         } else {
             if (!(result instanceof Cell)) {
-                throw KExceptionManager.internalError(
-                        "Expecting Cell, but got " + node.getClass() + " in Configuration Cleaner.",
-                        this, node);
-            } else    node = (Cell) result;
+                throw expectedCellError(node);
+            } else {
+                node = (Cell) result;
+            }
         }
         node.setDefaultAttributes();
         node.setEllipses(Ellipses.NONE);
