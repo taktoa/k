0499a84c0a32d2c5afe29b539347f9639bb56688
Remy Goldschmidt <taktoa@gmail.com>
2015-05-26 14:43:27 -0500

    removed commented out code from RemoveComments.java

--- func-backend/src/main/java/org/kframework/backend/func/transformers/RemoveComments.java
+++ func-backend/src/main/java/org/kframework/backend/func/transformers/RemoveComments.java
@@ -2,7 +2,6 @@
 package org.kframework.backend.func.transformers;
 
 import org.kframework.kil.ASTNode;
-import org.kframework.kil.Bracket;
 import org.kframework.kil.LiterateModuleComment;
 import org.kframework.kil.loader.Context;
 import org.kframework.kil.visitors.CopyOnWriteTransformer;
@@ -11,7 +10,6 @@ import org.kframework.kil.visitors.CopyOnWriteTransformer;
  * Delete comments
  */
 public class RemoveComments extends CopyOnWriteTransformer {
-
     public RemoveComments(Context context) {
         super("Remove comments", context);
     }
@@ -20,25 +18,4 @@ public class RemoveComments extends CopyOnWriteTransformer {
     public ASTNode visit(LiterateModuleComment node, Void _void)  {
         return null;
     }
-
-//    @Override
-//    public ASTNode visit(ASTNode node, Void _void)  {
-//        System.out.print("NODE TYPE: ");
-//        System.out.println(node.getClass().toString());
-//        return node;
-//    }
-
-//    @Override
-//    public ASTNode visit(Bracket node, Void _void)  {
-//        System.out.println("Remove: " + node.getFilename() + ":" + node.getLocation());
-//        return this.visitNode(node.getContent());
-//    }
-// 
-//    @Override
-//    public ASTNode visit(TermCons node, Void _void)  {
-//        System.out.println("Remove: " + node.getFilename() + ":" + node.getLocation());
-//        if (node.getProduction().containsAttribute("bracket"))
-//            return this.visitNode(node.getContents().get(0));
-//        return super.visit(node, _void);
-//    }
 }
