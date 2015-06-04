c5290ff075b9a340fc7796b1c9836d60dbd8dc31
Sebastian Conybeare <sebmathguy@gmail.com>
2015-06-04 00:16:25 -0500

    Create abstract NamespaceManager for unparsing

---
+++ func-backend/src/main/java/org/kframework/backend/genericAST/NamespaceManager.java
@@ -0,0 +1,12 @@
+// Copyright (c) 2013-2015 K Team. All Rights Reserved.
+package org.kframework.backend.genericAST;
+
+import org.kframework.backend.genericAST.ConstructorName;
+/**
+ * @author: Sebastian Conybeare
+ */
+public abstract class NamespaceManager {
+    
+    public abstract ConstructorName newConstructorName();
+
+}
