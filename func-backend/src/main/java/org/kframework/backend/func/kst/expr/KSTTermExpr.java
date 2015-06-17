6885a8f0966177ac9938367e38d9309d3228f098
Remy Goldschmidt <taktoa@gmail.com>
2015-06-16 20:56:49 -0500

    Added KSTMatch and KSTTermExpr

---
+++ func-backend/src/main/java/org/kframework/backend/func/kst/expr/KSTTermExpr.java
@@ -0,0 +1,34 @@
+package org.kframework.backend.func.kst;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public final class KSTTermExpr extends KSTExpr {
+    private final KSTTerm term;
+
+    public KSTTermExpr(KSTTerm term) {
+        this.term = term;
+    }
+
+    public KSTTerm getTerm() {
+        return term;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof KSTTermExpr) {
+            return o.hashCode() == hashCode();
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return term.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(expr %s)", term);
+    }
+}
