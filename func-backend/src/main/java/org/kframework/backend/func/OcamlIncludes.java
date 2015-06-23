9ed21ebf36577dffd9ec7a31f09bd1be536a2e1c
Remy Goldschmidt <taktoa@gmail.com>
2015-06-23 13:50:53 -0500

    Switch over to non-big_int based Ocaml includes

--- func-backend/src/main/java/org/kframework/backend/func/OcamlIncludes.java
+++ func-backend/src/main/java/org/kframework/backend/func/OcamlIncludes.java
@@ -1,3 +1,4 @@
+// Copyright (c) 2015 K Team. All Rights Reserved.
 package org.kframework.backend.func;
 
 import com.google.common.collect.ImmutableMap;
@@ -22,7 +23,7 @@ public final class OcamlIncludes {
     public static final String INT = encodeStringToIdentifier(Sort("Int"));
     private static long counter = 0;
 
-    private static final String kType =
+    public static final String kType =
         "t = kitem list\n" +
         " and kitem = KApply of klabel * t list\n" +
         "           | KToken of sort * string\n" +
@@ -30,13 +31,12 @@ public final class OcamlIncludes {
         "           | Map of t m\n" +
         "           | List of t list\n" +
         "           | Set of s\n" +
-        "           | Int of big_int\n" +
+        "           | Int of Z.t\n" +
         "           | String of string\n" +
         "           | Bool of bool\n" +
         "           | Bottom\n";
 
-    private static final String prelude =
-        "open Big_int\n" +
+    public static final String prelude =
         "module type S =\n" +
         "sig\n" +
         "  type 'a m\n" +
@@ -65,7 +65,7 @@ public final class OcamlIncludes {
         "    | (Map m1), (Map m2) -> (KMap.compare) compare m1 m2\n" +
         "    | (List l1), (List l2) -> compare_klist l1 l2\n" +
         "    | (Set s1), (Set s2) -> (KSet.compare) s1 s2\n" +
-        "    | (Int i1), (Int i2) -> compare_big_int i1 i2\n" +
+        "    | (Int i1), (Int i2) -> Z.compare i1 i2\n" +
         "    | (String s1), (String s2) -> Pervasives.compare s1 s2\n" +
         "    | (Bool b1), (Bool b2) -> if b1 = b2 then 0 else if b1 then -1 else 1\n" +
         "    | Bottom, Bottom -> 0\n" +
@@ -107,8 +107,8 @@ public final class OcamlIncludes {
         "  type t = Guard of int\n" +
         "  let compare c1 c2 = match c1 with Guard(i1) -> match c2 with Guard(i2) -> i2 - i1\n" +
         "end\n" +
-        "module Guard = Set.Make(GuardElt)\n";
-
+        "module Guard = Set.Make(GuardElt)\n" +
+        "let freshCounter : Z.t ref = ref Z.zero\n";
 
     private static final String midlude =
         "let eq k1 k2 = k1 = k2\n" +
@@ -116,7 +116,7 @@ public final class OcamlIncludes {
         "| ([" + TRUE + "]) -> true\n" +
         "| _ -> false\n" +
         "let rec list_range (c: k list * int * int) : k list = match c with\n" +
-        "| (l, 0, 0) -> l\n" +
+        "| (_, 0, 0) -> []\n" +
         "| (head :: tail, 0, len) -> head :: list_range(tail, 0, len - 1)\n" +
         "| (_ :: tail, n, len) -> list_range(tail, n - 1, len)\n" +
         "| ([], _, _) -> raise(Failure \"list_range\")\n" +
@@ -130,15 +130,17 @@ public final class OcamlIncludes {
         "| e1::e2::l -> print_kitem(e1) ^ \" ~> \" ^ print_k(e2::l)\n" +
         "and print_kitem(c: kitem) : string = match c with\n" +
         "| KApply(klabel, klist) -> print_klabel(klabel) ^ \"(\" ^ print_klist(klist) ^ \")\"\n" +
-        "| KToken(sort, s) -> \"#token(\\\"\" ^ s ^ \"\\\", \" ^ print_sort_string(sort) ^ \")\"\n" +
+        "| KToken(sort, s) -> \"#token(\\\"\" ^ (String.escaped s) ^ \"\\\", \\\"\" ^ print_sort(sort) ^ \"\\\")\"\n" +
         "| InjectedKLabel(klabel) -> \"#klabel(\" ^ print_klabel(klabel) ^ \")\"\n" +
         "| Bool(b) -> print_kitem(KToken(" + BOOL + ", string_of_bool(b)))\n" +
-        "| String(s) -> print_kitem(KToken(" + STRING + ", \"\\\"\" ^ s ^ \"\\\"\"))\n" +
-        "| Int(i) -> print_kitem(KToken(" + INT + ", string_of_big_int(i)))\n" +
+        "| String(s) -> print_kitem(KToken(" + STRING + ", \"\\\"\" ^ (String.escaped s) ^ \"\\\"\"))\n" +
+        "| Int(i) -> print_kitem(KToken(" + INT + ", Z.to_string(i)))\n" +
         "| Bottom -> \"`#Bottom`(.KList)\"\n" +
         "| List(l) -> List.fold_left (fun s k -> \"`_List_`(`ListItem`(\" ^ print_k(k) ^ \"),\" ^ s ^ \")\") \"`.List`(.KList)\" l\n" +
         "| Set(s) -> KSet.fold (fun k s -> \"`_Set_`(`SetItem`(\" ^ print_k(k) ^ \"), \" ^ s ^ \")\") s \"`.Set`(.KList)\"\n" +
-        "| Map(m) -> KMap.fold (fun k v s -> \"`_Map_`(`_|->_`(\" ^ print_k(k) ^ \", \" ^ print_k(v) ^ \"), \" ^ s ^ \")\") m \"`.Map`(.KList)\"\n";
+        "| Map(m) -> KMap.fold (fun k v s -> \"`_Map_`(`_|->_`(\" ^ print_k(k) ^ \", \" ^ print_k(v) ^ \"), \" ^ s ^ \")\") m \"`.Map`(.KList)\"\n" +
+        "let print_set f s = print_string (print_kitem(Set s))\n" +
+        "let print_map f m = print_string (print_kitem(Map m))\n";
 
     private static final String postlude =
         "let run c n=\n" +
@@ -152,26 +154,33 @@ public final class OcamlIncludes {
 
     static {
         ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
-        builder.put("#INT:_%Int_", "[Int a] :: [Int b] :: [] -> [Int (mod_big_int a b)]");
-        builder.put("#INT:_+Int_", "[Int a] :: [Int b] :: [] -> [Int (add_big_int a b)]");
-        builder.put("#INT:_<=Int_", "[Int a] :: [Int b] :: [] -> [Bool (le_big_int a b)]");
-        builder.put("Map:_|->_", "k1 :: k2 :: [] -> [Map (KMap.add k1 k2 KMap.empty)]");
+        builder.put("Map:_|->_", "k1 :: k2 :: [] -> [Map (KMap.singleton k1 k2)]");
         builder.put("Map:.Map", "[] -> [Map KMap.empty]");
-        builder.put("Map:__", "([Map k1]) :: ([Map k2]) :: [] -> [Map (KMap.merge (fun k a b -> match a, b with None, None -> None | None, Some v | Some v, None -> Some v) k1 k2)]");
+        builder.put("Map:__", "([Map k1]) :: ([Map k2]) :: [] -> [Map (KMap.merge (fun k a b -> match a, b with None, None -> None | None, Some v | Some v, None -> Some v | Some v1, Some v2 when v1 = v2 -> Some v1) k1 k2)]");
         builder.put("Map:lookup", "[Map k1] :: k2 :: [] -> (try KMap.find k2 k1 with Not_found -> [Bottom])");
         builder.put("Map:update", "[Map k1] :: k :: v :: [] -> [Map (KMap.add k v k1)]");
         builder.put("Map:remove", "[Map k1] :: k2 :: [] -> [Map (KMap.remove k2 k1)]");
-        builder.put("Map:keys", "[Map k1] :: [] -> [Set (KMap.fold (fun key -> KSet.add) k1 KSet.empty)]");
+        builder.put("Map:keys", "[Map k1] :: [] -> [Set (KMap.fold (fun k v -> KSet.add k) k1 KSet.empty)]");
+        builder.put("Map:values", "[Map k1] :: [] -> [Set (KMap.fold (fun key -> KSet.add) k1 KSet.empty)]");
+        builder.put("Map:choice", "[Map k1] :: [] -> match KMap.choose k1 with (k, _) -> k");
+        builder.put("Map:updateAll", "([Map k1]) :: ([Map k2]) :: [] -> [Map (KMap.merge (fun k a b -> match a, b with None, None -> None | None, Some v | Some v, None | Some _, Some v -> Some v) k1 k2)]");
         builder.put("Set:in", "k1 :: [Set k2] :: [] -> [Bool (KSet.mem k1 k2)]");
         builder.put("Set:.Set", "[] -> [Set KSet.empty]");
-        builder.put("Set:SetItem", "kl -> [Set (KSet.add (KApply(lbl, kl) :: []) KSet.empty)]");
+        builder.put("Set:SetItem", "k :: [] -> [Set (KSet.singleton k)]");
         builder.put("Set:__", "[Set s1] :: [Set s2] :: [] -> [Set (KSet.union s1 s2)]");
         builder.put("Set:difference", "[Set k1] :: [Set k2] :: [] -> [Set (KSet.diff k1 k2)]");
+        builder.put("Set:inclusion", "[Set k1] :: [Set k2] :: [] -> [Bool (KSet.subset k1 k2)]");
+        builder.put("Set:intersection", "[Set k1] :: [Set k2] :: [] -> [Set (KSet.inter k1 k2)]");
+        builder.put("Set:choice", "[Set k1] :: [] -> KSet.choose k1");
         builder.put("List:.List", "[] -> [List []]");
-        builder.put("List:ListItem", "kl -> [List ([KApply(lbl, kl)] :: [])]");
+        builder.put("List:ListItem", "k :: [] -> [List [k]]");
         builder.put("List:__", "[List l1] :: [List l2] :: [] -> [List (l1 @ l2)]");
-        builder.put("List:get", "[List l1] :: [Int i] :: [] -> (try List.nth l1 (int_of_big_int i) with Failure \"nth\" -> [Bottom])");
-        builder.put("List:range", "[List l1] :: [Int i1] :: [Int i2] :: [] -> (try [List (list_range (l1, (int_of_big_int i1), (List.length(l1) - (int_of_big_int i2) - (int_of_big_int i1))))] with Failure \"list_range\" -> [Bottom])");
+        builder.put("List:in", "k1 :: [List k2] :: [] -> [Bool (List.mem k1 k2)]");
+        builder.put("List:get", "[List l1] :: [Int i] :: [] -> let i = Z.to_int i in (try if i >= 0 then List.nth l1 i else List.nth l1 ((List.length l1) + i) with Failure \"nth\" -> [Bottom])");
+        builder.put("List:range", "[List l1] :: [Int i1] :: [Int i2] :: [] -> (try [List (list_range (l1, (Z.to_int i1), (List.length(l1) - (Z.to_int i2) - (Z.to_int i1))))] with Failure \"list_range\" -> [Bottom])");
+        builder.put("Collection:size", "[List l] :: [] -> [Int (Z.of_int (List.length l))] " +
+                "| [Map m] :: [] -> [Int (Z.of_int (KMap.cardinal m))] " +
+                "| [Set s] :: [] -> [Int (Z.of_int (KSet.cardinal s))]");
         builder.put("MetaK:#sort", "[KToken (sort, s)] :: [] -> [String (print_sort(sort))] " +
                 "| [Int _] :: [] -> [String \"Int\"] " +
                 "| [String _] :: [] -> [String \"String\"] " +
@@ -182,15 +191,51 @@ public final class OcamlIncludes {
                 "| _ -> [String \"\"]");
         builder.put("#K-EQUAL:_==K_", "k1 :: k2 :: [] -> [Bool (eq k1 k2)]");
         builder.put("#BOOL:_andBool_", "[Bool b1] :: [Bool b2] :: [] -> [Bool (b1 && b2)]");
+        builder.put("#BOOL:_andThenBool_", "[Bool b1] :: [Bool b2] :: [] -> [Bool (b1 && b2)]");
+        builder.put("#BOOL:_orBool_", "[Bool b1] :: [Bool b2] :: [] -> [Bool (b1 || b2)]");
+        builder.put("#BOOL:_orElseBool_", "[Bool b1] :: [Bool b2] :: [] -> [Bool (b1 || b2)]");
         builder.put("#BOOL:notBool_", "[Bool b1] :: [] -> [Bool (not b1)]");
+        builder.put("#STRING:_+String_", "[String s1] :: [String s2] :: [] -> [String (s1 ^ s2)]");
+        builder.put("#STRING:_<String_", "[String s1] :: [String s2] :: [] -> [Bool ((String.compare s1 s2) < 0)]");
+        builder.put("#STRING:_<=String_", "[String s1] :: [String s2] :: [] -> [Bool ((String.compare s1 s2) <= 0)]");
+        builder.put("#STRING:_>String_", "[String s1] :: [String s2] :: [] -> [Bool ((String.compare s1 s2) > 0)]");
+        builder.put("#STRING:_>=String_", "[String s1] :: [String s2] :: [] -> [Bool ((String.compare s1 s2) >= 0)]");
+        builder.put("#STRING:chrChar", "[Int i] :: [] -> [String (String.make 1 (Char.chr (Z.to_int i)))]");
+        builder.put("#STRING:findString", "[String s1] :: [String s2] :: [Int i] :: [] -> try [Int (Z.of_int (Str.search_forward (Str.regexp_string s2) s1 (Z.to_int i)))] with Not_found -> [Int (Z.of_int (-1))]");
+        builder.put("#STRING:rfindString", "[String s1] :: [String s2] :: [Int i] :: [] -> try [Int (Z.of_int (Str.search_backward (Str.regexp_string s2) s1 (Z.to_int i)))] with Not_found -> [Int (Z.of_int (-1))]");
+        builder.put("#STRING:lengthString", "[String s] :: [] -> [Int (Z.of_int (String.length s))]");
+        builder.put("#STRING:substrString", "[String s] :: [Int i1] :: [Int i2] :: [] -> [String (String.sub s (Z.to_int i1) (Z.to_int (Z.add i1 i2)))]");
+        builder.put("#STRING:ordChar", "[String s] :: [] -> [Int (Z.of_int (Char.code (String.get s 0)))]");
+        builder.put("#INT:_%Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.rem a b)]");
+        builder.put("#INT:_+Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.add a b)]");
+        builder.put("#INT:_<=Int_", "[Int a] :: [Int b] :: [] -> [Bool (Z.leq a b)]");
+        builder.put("#INT:_&Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.logand a b)]");
+        builder.put("#INT:_*Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.mul a b)]");
+        builder.put("#INT:_-Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.sub a b)]");
+        builder.put("#INT:_/Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.div a b)]");
+        builder.put("#INT:_<<Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.shift_left a (Z.to_int b))]");
+        builder.put("#INT:_<Int_", "[Int a] :: [Int b] :: [] -> [Bool (Z.lt a b)]");
+        builder.put("#INT:_>=Int_", "[Int a] :: [Int b] :: [] -> [Bool (Z.geq a b)]");
+        builder.put("#INT:_>>Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.shift_right a (Z.to_int b))]");
+        builder.put("#INT:_>Int_", "[Int a] :: [Int b] :: [] -> [Bool (Z.gt a b)]");
+        builder.put("#INT:_^Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.pow a (Z.to_int b))]");
+        builder.put("#INT:_xorInt_", "[Int a] :: [Int b] :: [] -> [Int (Z.logxor a b)]");
+        builder.put("#INT:_|Int_", "[Int a] :: [Int b] :: [] -> [Int (Z.logor a b)]");
+        builder.put("#INT:absInt", "[Int a] :: [] -> [Int (Z.abs a)]");
+        builder.put("#INT:maxInt", "[Int a] :: [Int b] :: [] -> [Int (Z.max a b)]");
+        builder.put("#INT:minInt", "[Int a] :: [Int b] :: [] -> [Int (Z.min a b)]");
+        builder.put("#CONVERSION:int2string", "[Int i] :: [] -> [String (Z.to_string i)]");
+        builder.put("#CONVERSION:string2int", "[String s] :: [] -> [Int (Z.of_string s)]");
+        builder.put("#CONVERSION:string2base", "[String s] :: [Int i] :: [] -> [Int (Z.of_string_base (Z.to_int i) s)]");
+        builder.put("#FRESH:fresh", "[String sort] :: [] -> let res = freshFunction sort !freshCounter in freshCounter := Z.add !freshCounter Z.one; res");
         hooks = builder.build();
     }
 
     static {
         ImmutableMap.Builder<String, Function<String, String>> builder = ImmutableMap.builder();
         builder.put("#BOOL", s -> "(Bool " + s + ")");
-        builder.put("#INT", s -> "(Int (big_int_of_string \"" + s + "\"))");
-        builder.put("#STRING", s -> "(String " + StringUtil.enquoteCString(StringUtil.unquoteKString(s)) + ")");
+        builder.put("#INT", s -> "(Int (Z.of_string \"" + s + "\"))");
+        builder.put("#STRING", s -> "(String " + StringUtil.enquoteCString(StringUtil.unquoteKString(StringUtil.unquoteKString("\"" + s + "\""))) + ")");
         sortHooks = builder.build();
     }
 
