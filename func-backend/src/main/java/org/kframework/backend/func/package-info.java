958c32888eb71bdda0f7a63e5c8b4d61c6340424
Remy Goldschmidt <taktoa@gmail.com>
2015-06-26 12:15:08 -0500

    squashme

--- func-backend/src/main/java/org/kframework/backend/func/package-info.java
+++ func-backend/src/main/java/org/kframework/backend/func/package-info.java
@@ -8,25 +8,19 @@
  * <ul>
  * <li>     Q: What do I need to use the K Functional Backend </li>
  * <ul><li> A: For now, you will need the following programs/libraries installed: </li></ul>
- * <ul><ul><li>                                   Runtime/kompile-time dependencies:     </li></ul></ul>
- * <ul><ul><li><a href="https://ocaml.org">                                    OCaml </a></li></ul></ul>
- * <ul><ul><li><a href="https://forge.ocamlcore.org/projects/zarith">         zarith </a></li></ul></ul>
- * <ul><ul><li><a href="http://projects.camlcity.org/projects/findlib.html"> findlib </a></li></ul></ul>
- * <ul><ul><li>                                      K Framework build dependencies:     </li></ul></ul>
- * <ul><ul><li>                                                               Java 8     </li></ul></ul>
- * <ul><ul><li><a href="https://maven.apache.org">                      Apache Maven </a></li></ul></ul>
- * <ul><li>    Additionally, it is assumed that you are running a Unix-like system </li></ul>
- * <li>     Q:  </li>
- * <ul><li> A:  </li></ul>
- * <li>     Q:  </li>
- * <ul><li> A:  </li></ul>
- * <li>     Q:  </li>
- * <ul><li> A:  </li></ul>
- * <li>     Q:  </li>
- * <ul><li> A:  </li></ul>
+ * <ul><ul><li>                                                Runtime/kompile-time dependencies: </li></ul></ul>
+ * <ul><ul><ul><li><a href="https://ocaml.org">                                    OCaml </a></li></ul></ul></ul>
+ * <ul><ul><ul><li><a href="https://forge.ocamlcore.org/projects/zarith">         zarith </a></li></ul></ul></ul>
+ * <ul><ul><ul><li><a href="http://projects.camlcity.org/projects/findlib.html"> findlib </a></li></ul></ul></ul>
+ * <ul><ul><li>                                                   K Framework build dependencies: </li></ul></ul>
+ * <ul><ul><ul><li>                                                               Java 8     </li></ul></ul></ul>
+ * <ul><ul><ul><li><a href="https://maven.apache.org">                      Apache Maven </a></li></ul></ul></ul>
+ * <ul><ul><li>               Additionally, it is assumed that you are running a Unix-like system </li></ul></ul>
+ * <!-- <li>     Q:  </li> -->
+ * <!-- <ul><li> A:  </li></ul> -->
  * </ul>
- * <br>
- * <br>
- * Copyright (c) 2015 K Team. All Rights Reserved.
+ * <br />
+ * <br />
+ * Copyright &copy; 2015 K Team. All Rights Reserved.
  */
 package org.kframework.backend.func;
