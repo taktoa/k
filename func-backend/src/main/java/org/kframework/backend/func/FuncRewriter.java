fe0bb40f7de9d7d50d24653b0755e7ea23996c53
Remy Goldschmidt <taktoa@gmail.com>
2015-06-26 12:03:12 -0500

    More cleanup

--- func-backend/src/main/java/org/kframework/backend/func/FuncRewriter.java
+++ func-backend/src/main/java/org/kframework/backend/func/FuncRewriter.java
@@ -4,10 +4,12 @@ import com.google.inject.Inject;
 import com.google.common.collect.Lists;
 
 import java.io.IOException;
+import java.io.File;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.BiFunction;
 
 import org.kframework.Rewriter;
 import org.kframework.attributes.Source;
@@ -17,7 +19,6 @@ import org.kframework.kompile.CompiledDefinition;
 import org.kframework.kompile.KompileOptions;
 import org.kframework.kore.K;
 import org.kframework.main.GlobalOptions;
-import org.kframework.utils.errorsystem.KEMException;
 import org.kframework.utils.errorsystem.KExceptionManager;
 import org.kframework.utils.file.FileUtil;
 import org.kframework.utils.inject.DefinitionScoped;
@@ -29,14 +30,34 @@ import static org.kframework.backend.func.FuncUtil.*;
  */
 @DefinitionScoped
 public class FuncRewriter implements Function<Module, Rewriter> {
-
     private final KExceptionManager kem;
     private final FileUtil files;
     private final CompiledDefinition def;
     private final DefinitionToFunc converter;
 
-    // The packages used by the generated OCaml
     private static final String ocamlPackages = "zarith,str";
+    private static final String pgmFileName   = "pgm.ml";
+
+    private final File
+        compileDirectory, compileOutFile, compileErrFile,
+        runtimeDirectory, runtimeOutFile, runtimeErrFile,
+        kompileDirectory, kompileDefFile;
+
+    private static final String
+        compileDirectoryName, compileOutFileName, compileErrFileName,
+        runtimeDirectoryName, runtimeOutFileName, runtimeErrFileName,
+        binaryOutputFileName, kompileDefFileName;
+
+    static {
+        compileDirectoryName = ".";
+        compileOutFileName   = "compile.out";
+        compileErrFileName   = "compile.err";
+        runtimeDirectoryName = ".";
+        runtimeOutFileName   = "run.out";
+        runtimeErrFileName   = "run.err";
+        binaryOutputFileName = "./a.out";
+        kompileDefFileName   = "def.cmo";
+    }
 
     // If you don't use ocamlfind, you will want to change this
     private static final String[] ocamlCmd = new String[]{
@@ -59,74 +80,166 @@ public class FuncRewriter implements Function<Module, Rewriter> {
         this.files = files;
         this.def = def;
         this.converter = new DefinitionToFunc(kem, files, globalOptions, kompileOptions);
+
+        this.compileDirectory = files.resolveTemp(compileDirectoryName);
+        this.compileErrFile   = files.resolveTemp(compileErrFileName);
+        this.compileOutFile   = files.resolveTemp(compileOutFileName);
+        this.runtimeDirectory = files.resolveTemp(runtimeDirectoryName);
+        this.runtimeErrFile   = files.resolveTemp(runtimeErrFileName);
+        this.runtimeOutFile   = files.resolveTemp(runtimeOutFileName);
+
+        this.kompileDirectory = files.resolveKompiled(".");
+        this.kompileDefFile   = files.resolveKompiled(kompileDefFileName);
+
         converter.convert(def);
     }
 
     @Override
     public Rewriter apply(Module module) {
         if(!module.equals(def.executionModule())) {
-            throw KEMException.criticalError("Invalid module specified for rewriting.\n" +
-                                             "Functional backend only supports rewriting\n" +
-                                             "over the definition's main module.");
+            nonExecutionModuleError();
         }
+
         return new Rewriter() {
             @Override
             public K execute(K k, Optional<Integer> depth) {
-                String ocaml = converter.convert(k, depth.orElse(-1));
-                files.saveToTemp("pgm.ml", ocaml);
-                try {
-
-                    ProcessBuilder pb = files.getProcessBuilder();
-                    int padding = 10;
-                    List<String> pbList = Lists.newArrayListWithCapacity(ocamlCmd.length + padding);
-                    pbList.addAll(Arrays.asList(ocamlCmd));
-                    pbList.add("-I");
-                    pbList.add(files.resolveKompiled(".").getAbsolutePath());
-                    pbList.add(files.resolveKompiled("def.cmo").getAbsolutePath());
-                    pbList.add("pgm.ml");
-                    String[] pbArr = new String[pbList.size()];
-                    for(int i = 0; i < pbList.size(); i++) {
-                        pbArr[i] = pbList.get(i);
-                    }
-
-                    pb = pb.command(pbArr);
-
-                    Process p = pb.directory(files.resolveTemp("."))
-                                  .redirectError(files.resolveTemp("compile.err"))
-                                  .redirectOutput(files.resolveTemp("compile.out"))
-                                  .start();
-
-                    int exit = p.waitFor();
-                    if(exit != 0) {
-                        System.err.println(files.loadFromTemp("compile.err"));
-                        throw KEMException.criticalError("Failed to compile program to ocaml.\n" +
-                                                         "See output for error information.");
-                    }
-                    p = files.getProcessBuilder()
-                             .command("./a.out")
-                             .directory(files.resolveTemp("."))
-                             .redirectError(files.resolveTemp("run.err"))
-                             .redirectOutput(files.resolveTemp("run.out"))
-                             .start();
-                    exit = p.waitFor();
-                    if(exit != 0) {
-                        System.err.println(files.loadFromTemp("run.err"));
-                        throw KEMException.criticalError("Failed to execute program in ocaml.\n" +
-                                                         "Rerun with --debug and examine the\n" +
-                                                         "temporary directory for information");
-                    }
-                    String output = files.loadFromTemp("run.out");
-                    return def.getParser(def.getParsedDefinition().getModule("KSEQ-SYMBOLIC").get(),
-                                         Sorts.K(),
-                                         kem)
-                              .apply(output, Source.apply("generated by ocaml output"));
-                } catch (IOException e) {
-                    throw KEMException.criticalError("Failed to start ocamlopt: " + e.getMessage(), e);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    throw KEMException.criticalError("Ocaml process interrupted.", e);
-                }
+                return executeOCaml(k, depth);
             }
         };
+    }
+
+    private K executeOCaml(K k, Optional<Integer> depth) {
+        generateOCaml(k, depth);
+        compileOCaml();
+        runOCaml();
+        return parseOCamlOutput();
+    }
+
+    private void generateOCaml(K k, Optional<Integer> depth) {
+        String ocaml = converter.convert(k, depth.orElse(-1));
+        files.saveToTemp(pgmFileName, ocaml);
+    }
+
+    private void compileOCaml() {
+        try {
+            Process p = startCompileProcess();
+
+            try {
+                int exit = p.waitFor();
+                if(exit != 0) { compileFailedError(exit); }
+            } catch(InterruptedException e) {
+                compileInterruptedError(e);
+            }
+        } catch(IOException e) {
+            compileIOError(e);
+        }
+    }
+
+    private void runOCaml() {
+        try {
+            Process p = startRuntimeProcess();
+
+            try {
+                int exit = p.waitFor();
+                if(exit != 0) { runtimeFailedError(exit); }
+            } catch(InterruptedException e) {
+                runtimeInterruptedError(e);
+            }
+        } catch(IOException e) {
+            runtimeIOError(e);
+        }
+    }
+
+    private K parseOCamlOutput() {
+        String output = files.loadFromTemp(runtimeOutFileName);
+        Module kseqSymbolic = def.getParsedDefinition().getModule("KSEQ-SYMBOLIC").get();
+        BiFunction<String, Source, K> parser = def.getParser(kseqSymbolic, Sorts.K(), kem);
+        Source ocamlSrc = Source.apply("generated by OCaml output");
+
+        return parser.apply(output, ocamlSrc);
+    }
+
+    private Process startRuntimeProcess() throws IOException {
+        return startProcess(files.getProcessBuilder(),
+                            runtimeDirectory,
+                            runtimeErrFile,
+                            runtimeOutFile,
+                            getRuntimeCommandLine());
+    }
+
+    private Process startCompileProcess() throws IOException {
+        return startProcess(files.getProcessBuilder(),
+                            compileDirectory,
+                            compileErrFile,
+                            compileOutFile,
+                            getCompileCommandLine());
+    }
+
+    private static Process startProcess(ProcessBuilder pb,
+                                        File dir,
+                                        File stderr,
+                                        File stdout,
+                                        String... cmd) throws IOException {
+        return pb.command(cmd)
+                 .directory(dir)
+                 .redirectError(stderr)
+                 .redirectOutput(stdout)
+                 .start();
+    }
+
+    private String[] getCompileCommandLine() {
+        int pbListLength = ocamlCmd.length + 10; // padding
+        List<String> pbList = Lists.newArrayListWithCapacity(pbListLength);
+
+        pbList = FuncUtil.addMany(pbList, ocamlCmd);
+        pbList.add("-I");
+        pbList.add(kompileDirectory.getAbsolutePath());
+        pbList.add(kompileDefFile.getAbsolutePath());
+        pbList.add(pgmFileName);
+
+        return pbList.toArray(new String[pbList.size()]);
+    }
+
+    private String[] getRuntimeCommandLine() {
+        return new String[]{ binaryOutputFileName };
+    }
+
+    private void nonExecutionModuleError() {
+        throw kemCriticalErrorF("Invalid module specified for rewriting.\n" +
+                                "Functional backend only supports rewriting\n" +
+                                "over the definition's main module.");
+    }
+
+    private void compileFailedError(int exitCode) {
+        System.err.println(files.loadFromTemp(compileErrFileName));
+        throw kemCriticalErrorF("Failed to compile program to OCaml.\n" +
+                                "See output for error information.\n" +
+                                "OCaml exit code: %d\n", exitCode);
+    }
+
+    private void runtimeFailedError(int exitCode) {
+        System.err.println(files.loadFromTemp(runtimeErrFileName));
+        throw kemCriticalErrorF("Failed to execute program in OCaml.\n" +
+                                "Rerun with --debug and examine the\n" +
+                                "temporary directory for information\n" +
+                                "Program exit code: %d\n", exitCode);
+    }
+
+    private void compileInterruptedError(InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw kemCriticalErrorF(e, "OCaml compile process interrupted.");
+    }
+
+    private void runtimeInterruptedError(InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw kemCriticalErrorF(e, "Generated OCaml code interrupted.");
+    }
+
+    private void compileIOError(IOException e) {
+        throw kemCriticalErrorF(e, "Failed to run OCaml compiler: %s", e.getMessage());
+    }
+
+    private void runtimeIOError(IOException e) {
+        throw kemCriticalErrorF(e, "Failed to run generated OCaml program: %s", e.getMessage());
     }
 }
--- func-backend/src/main/java/org/kframework/backend/func/FuncUtil.java
+++ func-backend/src/main/java/org/kframework/backend/func/FuncUtil.java
@@ -1,9 +1,17 @@
 // Copyright (c) 2015 K Team. All Rights Reserved.
 package org.kframework.backend.func;
 
+import com.google.common.collect.Lists;
+
 import java.util.List;
 import java.util.Collection;
-import com.google.common.collect.Lists;
+
+import org.kframework.kore.K;
+import org.kframework.definition.Sentence;
+import org.kframework.parser.ProductionReference;
+import org.kframework.attributes.Source;
+import org.kframework.attributes.Location;
+import org.kframework.utils.errorsystem.KEMException;
 
 /**
  * Utility functions for use in
@@ -13,76 +21,54 @@ import com.google.common.collect.Lists;
 public final class FuncUtil {
     private FuncUtil() {}
 
-    public static <T, C extends Collection<T>, A extends T> void addArray(C col, A[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            col.add(arr[i]);
+
+    // ------------------------------------------------------------------------
+    // ---------------------------- List functions ----------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Add all the elements of an array {@code A[]} to a given
+     * {@code ? extends Collection<T>} where {@code A extends T}.
+     * Any underlying ordering to the given collection will be respected,
+     * assuming that that collection implements {@link Collection#addAll addAll}
+     * in a way that respects ordering.
+     * Should run in <code>O(m + n)</code> time and <code>O(m + n)</code> space,
+     * where <code>m = col.size()</code> and <code>n = arr.length</code>
+     * Should be thread-safe (it synchronizes on {@code col} and {@code arr}).
+     */
+     public static <T, C extends Collection<T>, A extends T> C addMany(C col,
+                                                                       A... arr) {
+        List<T> al = Lists.newArrayListWithCapacity(col.size() + arr.length + 1);
+
+        synchronized(col) {
+            al.addAll(col);
         }
-    }
 
-    /**
-     * An alias so we don't have System.out.* everywhere
-     * @param fmt   the format string to pass to System.out.format
-     * @param args  the Objects to pass to System.out.format
-     */
-    public static void outprintf(String fmt, Object... args) {
-        System.out.format(fmt, args);
-    }
+        synchronized(arr) {
+            for(int i = 0; i < arr.length; i++) {
+                al.add(arr[i]);
+            }
+        }
 
-    /**
-     * This is just outprintf with a newline added to
-     * the end of the format string
-     * @param fmt   the format string to pass to System.out.format
-     * @param args  the Objects to pass to System.out.format
-     */
-    public static void outprintfln(String fmt, Object... args) {
-        outprintf(fmt + "\n", args);
-    }
-
-    /**
-     * In case you misspell it, we have this version, so it will give
-     * a deprecation warning at compile time.
-     * @deprecated use {@link #outprintfln()} instead
-     * @param fmt   the format string to pass to System.out.format
-     * @param args  the Objects to pass to System.out.format
-     */
-    @Deprecated
-    public static void outprintlnf(String fmt, Object... args) {
-        outprintfln(fmt, args);
-    }
-
-    /**
-     * An alias so we don't have System.err.* everywhere
-     * @param fmt   the format string to pass to System.err.format
-     * @param args  the Objects to pass to System.err.format
-     */
-    public static void errprintf(String fmt, Object... args) {
-        System.err.format(fmt, args);
-    }
-
-    /**
-     * This is just errprintf with a newline added to
-     * the end of the format string
-     * @param fmt   the format string to pass to System.err.format
-     * @param args  the Objects to pass to System.err.format
-     */
-    public static void errprintfln(String fmt, Object... args) {
-        errprintf(fmt + "\n", args);
-    }
-
-    /**
-     * In case you misspell it, we have this version, so it will give
-     * a deprecation warning at compile time.
-     * @deprecated  use {@link #errprintfln()} instead
-     * @param fmt   the format string to pass to System.err.format
-     * @param args  the Objects to pass to System.err.format
-     */
-    @Deprecated
-    public static void errprintlnf(String fmt, Object... args) {
-        errprintfln(fmt, args);
+        return (C) al;
     }
 
     /**
      * Outputs an ArrayList-backed List of Integers
+     * that represents a closed interval from {@code min} to {@code max} by {@code step}
+     * <br><br>
+     * For some <code> r = rangeInclusive(a, &Delta;, b) </code>
+     * and <code> s = r.size() </code>
+     * <style type='text/css'> .eqn { margin-bottom: 20px; } </style>
+     * <ol>
+     * <li class="eqn"><pre> s            === 1 + ((b - a) / &Delta;) </pre></li>
+     * <li class="eqn"><pre> r.get(0)     === a                       </pre></li>
+     * <li class="eqn"><pre> r.get(i)     === (i * &Delta;) + a       </pre></li>
+     * <li class="eqn"><pre> r.get(s - 1) === b                       </pre></li>
+     * </ol>
+     * @param min   bottom end of the range
+     * @param step  difference between any two consecutive elements
+     * @param max   top end of the range
      */
     public static List<Integer> rangeInclusive(int min, int step, int max) {
         int elements = Math.abs((max - min) / step);
@@ -115,5 +101,190 @@ public final class FuncUtil {
 
     public static List<Integer> rangeExclusive(int max) {
         return rangeExclusive(0, max);
+    }
+
+
+    // ------------------------------------------------------------------------
+    // ------------------------- System.out.* aliases -------------------------
+    // ------------------------------------------------------------------------
+
+
+    /**
+     * An alias so we don't have System.out.* everywhere
+     * @param fmt   the format string to pass to System.out.format
+     * @param args  the Objects to pass to System.out.format
+     */
+    public static void outprintf(String fmt, Object... args) {
+        System.out.format(fmt, args);
+    }
+
+    /**
+     * This is just outprintf with a newline added to
+     * the end of the format string
+     * @param fmt   the format string to pass to System.out.format
+     * @param args  the Objects to pass to System.out.format
+     */
+    public static void outprintfln(String fmt, Object... args) {
+        outprintf(fmt + "\n", args);
+    }
+
+    /**
+     * In case you misspell it, we have this version, so it will give
+     * a deprecation warning at compile time.
+     * @deprecated  Use {@link #outprintfln() outprintfln} instead
+     * @param fmt   the format string to pass to System.out.format
+     * @param args  the Objects to pass to System.out.format
+     */
+    @Deprecated
+    public static void outprintlnf(String fmt, Object... args) {
+        outprintfln(fmt, args);
+    }
+
+    /**
+     * An alias so we don't have System.err.* everywhere
+     * @param fmt   the format string to pass to System.err.format
+     * @param args  the Objects to pass to System.err.format
+     */
+    public static void errprintf(String fmt, Object... args) {
+        System.err.format(fmt, args);
+    }
+
+    /**
+     * This is just errprintf with a newline added to
+     * the end of the format string
+     * @param fmt   the format string to pass to System.err.format
+     * @param args  the Objects to pass to System.err.format
+     */
+    public static void errprintfln(String fmt, Object... args) {
+        errprintf(fmt + "\n", args);
+    }
+
+    /**
+     * In case you misspell it, we have this version, so it will give
+     * a deprecation warning at compile time.
+     * @deprecated  Use {@link #errprintfln() errprintfln} instead
+     * @param fmt   the format string to pass to System.err.format
+     * @param args  the Objects to pass to System.err.format
+     */
+    @Deprecated
+    public static void errprintlnf(String fmt, Object... args) {
+        errprintfln(fmt, args);
+    }
+
+
+    // ------------------------------------------------------------------------
+    // ------------------------- KEMException aliases -------------------------
+    // ------------------------------------------------------------------------
+
+
+    // ------------------ KEMException.compilerError aliases ------------------
+
+    /** Helper function for {@link KEMException#compilerError(String) compilerError} */
+    public static KEMException kemCompilerErrorF(String fmt, Object... obj) {
+        return KEMException.compilerError(String.format(fmt, obj));
+    }
+
+    /** Helper function for {@link KEMException#compilerError(String, Throwable) compilerError} */
+    public static KEMException kemCompilerErrorF(Throwable e, String fmt, Object... obj) {
+        return KEMException.compilerError(String.format(fmt, obj), e);
+    }
+
+    /** Helper function for {@link KEMException#compilerError(String, K) compilerError} */
+    public static KEMException kemCompilerErrorF(K node, String fmt, Object... obj) {
+        return KEMException.compilerError(String.format(fmt, obj), node);
+    }
+
+    /** Helper function for {@link KEMException#compilerError(String, Sentence) compilerError} */
+    public static KEMException kemCompilerErrorF(Sentence node, String fmt, Object... obj) {
+        return KEMException.compilerError(String.format(fmt, obj), node);
+    }
+
+    // ------------------ KEMException.criticalError aliases ------------------
+
+    /** Helper function for {@link KEMException#criticalError(String) criticalError} */
+    public static KEMException kemCriticalErrorF(String fmt, Object... obj) {
+        return KEMException.criticalError(String.format(fmt, obj));
+    }
+
+    /** Helper function for {@link KEMException#criticalError(String, Throwable) criticalError} */
+    public static KEMException kemCriticalErrorF(Throwable e, String fmt, Object... obj) {
+        return KEMException.criticalError(String.format(fmt, obj), e);
+    }
+
+    /** Helper function for {@link KEMException#criticalError(String, K) criticalError} */
+    public static KEMException kemCriticalErrorF(K node, String fmt, Object... obj) {
+        return KEMException.criticalError(String.format(fmt, obj), node);
+    }
+
+    /** Helper function for {@link KEMException#criticalError(String, Sentence) criticalError} */
+    public static KEMException kemCriticalErrorF(Sentence node, String fmt, Object... obj) {
+        return KEMException.criticalError(String.format(fmt, obj), node);
+    }
+
+    /** Helper function for {@link KEMException#criticalError(String, ProductionReference) criticalError} */
+    public static KEMException kemCriticalErrorF(ProductionReference node, String fmt, Object... obj) {
+       return KEMException.criticalError(String.format(fmt, obj), node);
+    }
+
+    /**
+     * Helper function for
+     * {@link KEMException#criticalError(String, Throwable, Location, Source) criticalError}
+     */
+    public static KEMException kemCriticalErrorF(Throwable e,
+                                                 Location loc,
+                                                 Source src,
+                                                 String fmt,
+                                                 Object... obj) {
+        return KEMException.criticalError(String.format(fmt, obj), e, loc, src);
+    }
+
+    // ----------------- KEMException.innerParserError aliases ----------------
+
+    /** Helper function for {@link KEMException#innerParserError(String) innerParserError} */
+    public static KEMException kemInnerParserErrorF(String fmt, Object... obj) {
+        return KEMException.innerParserError(String.format(fmt, obj));
+    }
+
+    /**
+     * Helper function for
+     * {@link KEMException#innerParserError(String, Throwable, Location, Source) innerParserError}
+     */
+    public static KEMException kemInnerParserErrorF(Throwable e,
+                                                    Location loc,
+                                                    Source src,
+                                                    String fmt,
+                                                    Object... obj) {
+        return KEMException.innerParserError(String.format(fmt, obj), e, src, loc);
+    }
+
+    // ------------------ KEMException.internalError aliases ------------------
+
+    /** Helper function for {@link KEMException#internalError(String) internalError} */
+    public static KEMException kemInternalErrorF(String fmt, Object... obj) {
+        return KEMException.internalError(String.format(fmt, obj));
+    }
+
+    /** Helper function for {@link KEMException#internalError(String, Throwable) internalError} */
+    public static KEMException kemInternalErrorF(Throwable e, String fmt, Object... obj) {
+        return KEMException.internalError(String.format(fmt, obj), e);
+    }
+
+    /** Helper function for {@link KEMException#internalError(String, K) internalError} */
+    public static KEMException kemInternalErrorF(K node, String fmt, Object... obj) {
+        return KEMException.internalError(String.format(fmt, obj), node);
+    }
+
+    // ----------------- KEMException.outerParserError aliases ----------------
+
+    /**
+     * Helper function for
+     * {@link KEMException#outerParserError(String, Throwable, Location, Source) outerParserError}
+     */
+    public static KEMException kemOuterParserErrorF(Throwable e,
+                                                    Location loc,
+                                                    Source src,
+                                                    String fmt,
+                                                    Object... obj) {
+        return KEMException.outerParserError(String.format(fmt, obj), e, src, loc);
     }
 }
