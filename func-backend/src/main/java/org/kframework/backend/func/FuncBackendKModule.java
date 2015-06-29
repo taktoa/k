18961d402da1b5b014aa91b4aa2c207d50d40d36
Remy Goldschmidt <taktoa@gmail.com>
2015-06-29 16:07:19 -0500

    Cleanup FuncBackend/FuncRewriter/FuncBackendKModule

--- func-backend/src/main/java/org/kframework/backend/func/FuncBackendKModule.java
+++ func-backend/src/main/java/org/kframework/backend/func/FuncBackendKModule.java
@@ -2,6 +2,7 @@ package org.kframework.backend.func;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
+import com.google.inject.Binder;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.MapBinder;
 import org.kframework.Rewriter;
@@ -14,19 +15,17 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
+ * Dependency injection for the functional backend
+ *
  * @author: Remy Goldschmidt
  */
 public class FuncBackendKModule extends AbstractKModule {
-
     @Override
     public List<Module> getKompileModules() {
         return Collections.singletonList(new AbstractModule() {
             @Override
             protected void configure() {
-
-                MapBinder<String, Consumer<CompiledDefinition>> mapBinder = MapBinder.newMapBinder(
-                        binder(), TypeLiteral.get(String.class), new TypeLiteral<Consumer<CompiledDefinition>>() {});
-                mapBinder.addBinding("func").to(FuncBackend.class);
+                configureKompileModules(binder());
             }
         });
     }
@@ -36,12 +35,24 @@ public class FuncBackendKModule extends AbstractKModule {
         return Collections.singletonList(new AbstractModule() {
             @Override
             protected void configure() {
-
-                MapBinder<String, Function<org.kframework.definition.Module, Rewriter>> rewriterBinder = MapBinder.newMapBinder(
-                        binder(), TypeLiteral.get(String.class), new TypeLiteral<Function<org.kframework.definition.Module, Rewriter>>() {
-                        });
-                rewriterBinder.addBinding("func").to(FuncRewriter.class);
+                configureDefinitionSpecificKRunModules(binder());
             }
         });
+    }
+
+    private void configureKompileModules(Binder binder) {
+        MapBinder<String, Consumer<CompiledDefinition>> mapBinder;
+        mapBinder = MapBinder.newMapBinder(binder,
+                                           TypeLiteral.get(String.class),
+                                           new TypeLiteral<Consumer<CompiledDefinition>>() {});
+        mapBinder.addBinding("func").to(FuncBackend.class);
+    }
+
+    private void configureDefinitionSpecificKRunModules(Binder binder) {
+        MapBinder<String, Function<org.kframework.definition.Module, Rewriter>> rewriterBinder;
+        rewriterBinder = MapBinder.newMapBinder(binder,
+                                                TypeLiteral.get(String.class),
+                                                new TypeLiteral<Function<org.kframework.definition.Module, Rewriter>>() {});
+        rewriterBinder.addBinding("func").to(FuncRewriter.class);
     }
 }
